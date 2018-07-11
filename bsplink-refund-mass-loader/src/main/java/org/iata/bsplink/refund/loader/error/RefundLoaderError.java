bfe19e6a85269f06cbd1893be45c509c412906c5
Jenkins NFE user <jenkins@jenkins.nfedev.accelya.com>
2018-07-11 16:29:39 +0200

    Synchronize repository bsplink-refund-mass-loader
    Jenkins job: jenkins-Accelya-bsplink-refund-mass-loader-43

--- bsplink-refund-mass-loader/src/main/java/org/iata/bsplink/refund/loader/error/RefundLoaderError.java
+++ bsplink-refund-mass-loader/src/main/java/org/iata/bsplink/refund/loader/error/RefundLoaderError.java
@@ -2,13 +2,11 @@ package org.iata.bsplink.refund.loader.error;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import org.iata.bsplink.refund.loader.model.record.RecordIdentifier;
 
 @Getter
 @Setter
-@ToString
 public class RefundLoaderError {
     String message;
     String transactionNumber;
--- bsplink-refund-mass-loader/src/main/java/org/iata/bsplink/refund/loader/job/RefundItemProcessor.java
+++ bsplink-refund-mass-loader/src/main/java/org/iata/bsplink/refund/loader/job/RefundItemProcessor.java
@@ -1,10 +1,7 @@
 package org.iata.bsplink.refund.loader.job;
 
-import java.util.List;
-
 import org.iata.bsplink.refund.loader.creator.RefundCreator;
 import org.iata.bsplink.refund.loader.dto.Refund;
-import org.iata.bsplink.refund.loader.error.RefundLoaderError;
 import org.iata.bsplink.refund.loader.model.RefundDocument;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,9 +15,6 @@ public class RefundItemProcessor implements ItemProcessor<RefundDocument, Refund
 
     @Autowired
     RefundCreator refundCreator;
-
-    @Autowired
-    List<RefundLoaderError> refundLoaderErrors;
 
     @Override
     public Refund process(RefundDocument item) throws Exception {
