56e473d6749e40f4acf0952bec5de5618b4916ea
Jenkins NFE user <jenkins@jenkins.nfedev.accelya.com>
2018-07-12 13:32:39 +0200

    Synchronize repository bsplink-refund-mass-loader
    Jenkins job: jenkins-Accelya-bsplink-refund-mass-loader-48

--- bsplink-refund-mass-loader/src/main/java/org/iata/bsplink/refund/loader/dto/Contact.java
+++ bsplink-refund-mass-loader/src/main/java/org/iata/bsplink/refund/loader/dto/Contact.java
@@ -2,9 +2,11 @@ package org.iata.bsplink.refund.loader.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class Contact {
 
     private String contactName;
--- bsplink-refund-mass-loader/src/main/java/org/iata/bsplink/refund/loader/dto/TaxMiscellaneousFee.java
+++ bsplink-refund-mass-loader/src/main/java/org/iata/bsplink/refund/loader/dto/TaxMiscellaneousFee.java
@@ -4,9 +4,11 @@ import java.math.BigDecimal;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class TaxMiscellaneousFee {
 
     private BigDecimal amount = BigDecimal.ZERO;
