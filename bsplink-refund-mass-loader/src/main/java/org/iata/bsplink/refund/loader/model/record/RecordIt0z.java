ab3ca6e49a5c3590672bc8e81335febb0377b4b9
Jenkins NFE user <jenkins@jenkins.nfedev.accelya.com>
2018-07-18 08:23:01 +0200

    Synchronize repository bsplink-refund-mass-loader
    Jenkins job: jenkins-Accelya-bsplink-refund-mass-loader-65

--- bsplink-refund-mass-loader/src/main/java/org/iata/bsplink/refund/loader/model/record/RecordIt0z.java
+++ bsplink-refund-mass-loader/src/main/java/org/iata/bsplink/refund/loader/model/record/RecordIt0z.java
@@ -5,9 +5,15 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
 @Getter
 @Setter
 @ToString
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class RecordIt0z implements Record {
 
     @Setter(AccessLevel.NONE)
