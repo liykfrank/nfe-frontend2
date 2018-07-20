ba64efaa479ab234ae70546c1a64b3612a67df68
Jenkins NFE user <jenkins@jenkins.nfedev.accelya.com>
2018-07-20 12:46:57 +0200

    Synchronize repository bsplink-agency-memo
    Jenkins job: jenkins-Accelya-bsplink-agency-memo-101

--- bsplink-agency-memo/src/main/java/org/iata/bsplink/agencymemo/controller/ReasonController.java
+++ bsplink-agency-memo/src/main/java/org/iata/bsplink/agencymemo/controller/ReasonController.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +26,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/reasons")
+@CrossOrigin(origins = "*", maxAge = 3600)
+
 public class ReasonController {
 
     @Autowired
