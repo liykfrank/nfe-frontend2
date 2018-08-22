43e11c48b6640cc6a6b595b5620deeeee899d3e3
Jenkins NFE user <jenkins@jenkins.nfedev.accelya.com>
2018-08-22 09:22:40 +0200

    Synchronize repository bsplink-agency-memo
    Jenkins job: jenkins-Accelya-bsplink-agency-memo-125

--- bsplink-agency-memo/src/main/java/org/iata/bsplink/agencymemo/configuration/SecurityConfig.java
+++ bsplink-agency-memo/src/main/java/org/iata/bsplink/agencymemo/configuration/SecurityConfig.java
@@ -50,9 +50,6 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        http.csrf().disable().authorizeRequests()
-                .antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
-                        "/configuration/security", "/swagger-ui.html", "/webjars/**")
-                .permitAll().anyRequest().authenticated();
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
     }
 }
