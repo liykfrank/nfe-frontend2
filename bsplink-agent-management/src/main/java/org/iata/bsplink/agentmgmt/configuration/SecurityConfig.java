f2d7ed7bbb2ec7c569d55dfdbdb45d21dc63dd51
Jenkins NFE user <jenkins@jenkins.nfedev.accelya.com>
2018-08-29 13:38:13 +0200

    Synchronize repository bsplink-agent-management
    Jenkins job: jenkins-Accelya-bsplink-agent-management-37

--- bsplink-agent-management/src/main/java/org/iata/bsplink/agentmgmt/configuration/SecurityConfig.java
+++ bsplink-agent-management/src/main/java/org/iata/bsplink/agentmgmt/configuration/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -51,9 +50,6 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
-                        "/configuration/security", "/swagger-ui.html", "/webjars/**")
-                .permitAll().anyRequest().authenticated();
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
     }
 }
