2ba4dd47d389ea86f7649679c33acdf66bfe68c4
Jenkins NFE user <jenkins@jenkins.nfedev.accelya.com>
2018-09-12 10:04:38 +0200

    Synchronize repository bsplink-user-management
    Jenkins job: jenkins-Accelya-bsplink-user-management-65

---
+++ bsplink-user-management/src/main/resources/db/migration/V0010__bsplink_options.sql
@@ -0,0 +1,40 @@
+
+
+insert into bsplink_user.bsplink_option (id) values ('CreateUser');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('CreateUser', 'BSP');
+
+insert into bsplink_user.bsplink_option (id) values ('CreateSubuser');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('CreateSubuser', 'AIRLINE');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('CreateSubuser', 'AGENT');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('CreateSubuser', 'BSP');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('CreateSubuser', 'DPC');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('CreateSubuser', 'GDS');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('CreateSubuser', 'IATA');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('CreateSubuser', 'THIRDPARTY');
+
+
+insert into bsplink_user.bsplink_option (id) values ('ModifyUser');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('ModifyUser', 'BSP');
+
+insert into bsplink_user.bsplink_option (id) values ('ModifySubuser');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('ModifySubuser', 'AIRLINE');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('ModifySubuser', 'AGENT');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('ModifySubuser', 'BSP');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('ModifySubuser', 'DPC');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('ModifySubuser', 'GDS');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('ModifySubuser', 'IATA');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('ModifySubuser', 'THIRDPARTY');
+
+insert into bsplink_user.bsplink_option (id) values ('JobMonitor');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('JobMonitor', 'BSP');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('JobMonitor', 'IATA');
+
+
+insert into bsplink_user.bsplink_option (id) values ('EditTemplate');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('EditTemplate', 'BSP');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('EditTemplate', 'IATA');
+
+insert into bsplink_user.bsplink_option (id) values ('QueryTemplate');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('QueryTemplate', 'BSP');
+insert into bsplink_user.bsplink_option_user_types (bsplink_option_id, user_types) values ('QueryTemplate', 'IATA');
+
